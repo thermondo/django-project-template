06cb3846ea8eac00f1a2f77160c9c55192cfeaa7
Til Boerner <tilman.boerner@thermondo.de>
2018-09-28 16:00:18 +0200

    Fix linting errors

--- django_project_template/project_name/urls.py
+++ django_project_template/project_name/urls.py
@@ -1,4 +1,4 @@
-"""testing URL Configuration
+"""URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -12,6 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
 from django.urls import path
